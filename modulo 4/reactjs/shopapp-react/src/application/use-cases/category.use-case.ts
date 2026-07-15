b9442cf6ac79298805c5f00803fe2461a7a226a2
Dayana Josselyn Pisco Aviles <josselynpisco1755@gmail.com>
2026-07-15 10:37:21 -0500

    SHOAPP WEB CHEKPOINT - 5-07-2026

---
+++ modulo 4/reactjs/shopapp-react/src/application/use-cases/category.use-case.ts
@@ -0,0 +1,20 @@
+// src/application/use-cases/category.use-case.ts
+import type { CategoryRepository } from '@/domain/ports/category.repository'
+import type { Category } from '@/domain/entities/category.entity'
+import type { CategoryStats } from '@/domain/entities/category-stats.entity'
+
+export class CategoryUseCase {
+  private readonly categoryRepository: CategoryRepository
+
+  constructor(categoryRepository: CategoryRepository) {
+    this.categoryRepository = categoryRepository
+  }
+
+  getCategories(): Promise<Category[]> {
+    return this.categoryRepository.getCategories()
+  }
+
+  getStats(): Promise<CategoryStats> {
+  return this.categoryRepository.getStats()
+}
+}
